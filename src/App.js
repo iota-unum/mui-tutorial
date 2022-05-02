0e6aff10b8a5d5426405d48be8f4ca2a1f6f14f4
luigi <luigilamastra@gmail.com>
2022-05-02 05:34:43 +0200

    typography

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button, Container, Paper, Stack } from '@mui/material';
+import { Button, Container, Paper, Stack, Typography } from '@mui/material';
 import './App.css';
 import MyButton from './styles/MyButton';
 
@@ -6,25 +6,27 @@ function App() {
   return (
     <div className='App'>
       <Container>
-        <Paper sx={{padding:'4em', }} >
-          <Stack spacing={2}>
+        <Typography variant='h1'>MUI Playground</Typography>
+        <Paper sx={{ padding: '4em' }}>
+          {/* <Stack spacing={2}>
             <input fullWidth placeholder='First Name' />
             <input fullWidth placeholder='First Name' />
             <input fullWidth placeholder='First Name' />
             <input fullWidth placeholder='First Name' />
-          </Stack>
+          </Stack> */}
+      <Stack direction='row' spacing={2} justifyContent='center' mt={3}>
+        <Button variant='contained' borderRadius='50%'>
+          {' '}
+          Submit{' '}
+        </Button>
+        <Button variant='contained' color='secondary'>
+          {' '}
+          Submit{' '}
+        </Button>
+        <MyButton variant='contained'>Custom</MyButton>
+      </Stack>
         </Paper>
-
-
       </Container>
-        <Stack direction='row' spacing={2} justifyContent='center' mt={3}> 
-        
-        <Button variant='contained' borderRadius='50%' > Submit </Button>
-        <Button variant='contained' color='secondary' > Submit </Button>
-    <MyButton variant='contained'>Custom</MyButton>
-        </Stack>
-
-      <h1>MUI Playground</h1>
     </div>
   );
 }
