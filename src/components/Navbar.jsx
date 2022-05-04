48f9be238c566b660c0ce32f9261e1ec974f1df3
luigi <luigilamastra@gmail.com>
2022-05-04 17:57:48 +0200

    feat: search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
-import { AppBar, styled, Toolbar, Typography } from '@mui/material';
+import { AppBar, InputBase, styled, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import PetsIcon from '@mui/icons-material/Pets';
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
   justifyContent: 'space-between',
 });
+
+const Search = styled('div')(({theme})=>({
+backgroundColor: 'white', 
+padding:' 0 1em',
+borderRadius: theme.shape.borderRadius,
+width: '40%'
+}))
+const Icons = styled('div')(({theme})=>({
+backgroundColor: 'white', 
+padding:'.5em',
+}))
 const Navbar = () => {
   return (
     <AppBar position='sticky'>
@@ -13,6 +24,10 @@ const Navbar = () => {
           LAMA DEV
         </Typography>
         <PetsIcon sx={{ display: { xs: 'block', sm: 'none' } }}></PetsIcon>
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons></Icons>
       </StyledToolbar>
     </AppBar>
   );
